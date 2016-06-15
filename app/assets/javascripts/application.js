cdf0e7dbbf533193867ca1087733fdc98a258ec3
JoshSutcliffe <joshclement12@gmail.com>
2016-06-15 12:59:52 +0100

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
+//= require agency
+//= require bootstrap
+//= require bootstrap.min
+//= require cbpAnimatedHeader
+//= require cbpAnimatedHeader.min
+//= require classie
+//= require contact_me
+//= require jqBootstrapValidation
